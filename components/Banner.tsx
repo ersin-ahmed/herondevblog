4b9e18f1155924a67e9d07a0bf5d06cca7fe9c2b
ersin-ahmed <ersinahmeddev@gmail.com>
2022-11-12 21:21:12 +0200

    fixed some deployment issues.

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -9,8 +9,8 @@ export default function Banner() {
 					is a place to write, read and connect
 				</h1>
 				<h2>
-					It's easy and free to post your thinking on any topic and connect with
-					millions of readers.
+					It&aposs easy and free to post your thinking on any topic and connect
+					with millions of readers.
 				</h2>
 			</div>
 
