d40929a942b9bb1efe5b6238a68e8a1fcc2f37cb
ersin-ahmed <ersinahmeddev@gmail.com>
2022-11-21 14:18:21 +0200

    Seo tweaks

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home({ posts }: Props) {
 				<title>HeronDev Blog</title>
 				<meta
 					name='description'
-					content='A Blog about Programming, Web Development and many other topics regarding Software Development and Engineering. Founded by Ersin Ahmed.'
+					content='A Blog about Programming, Web Development and many other topics regarding Software Development and Engineering. Founded by Ersin Ahmed (Web Developer from Bulgaria).'
 					key='desc'
 				/>
 				<link rel='icon' href='/images/logo-white.png' />
