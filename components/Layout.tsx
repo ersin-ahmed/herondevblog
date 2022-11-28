f0a049ca3e81ec2d8607fdf4b5284d98fe480ceb
ersin-ahmed <ersinahmeddev@gmail.com>
2022-11-28 23:06:34 +0200

    Took adsense script out

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,15 +1,9 @@
 import Header from './Header';
 import Footer from './Footer';
-import Script from 'next/script';
 
 export default function Layout({ children }) {
 	return (
 		<>
-			<Script
-				async
-				src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9115882332062777'
-				crossOrigin='anonymous'
-			></Script>
 			<Header />
 			<>{children}</>
 		</>
