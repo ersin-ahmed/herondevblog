bc7dd02440d8c469059625a0fe86b68c59f93347
ersin-ahmed <ersinahmeddev@gmail.com>
2022-12-04 18:58:10 +0200

    Changed layout of main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,23 +32,30 @@ export default function Home({ posts }: Props) {
 				<hr className='max-w-lg my-5 mx-auto border border-teal-600' />
 
 				{/*Featured Posts */}
-				<div className='bg-cyan-100 rounded-xl border-2 border-cyan-100 shadow-md grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6'>
+				{/* V1 */}
+				{/* <div className='bg-cyan-100 rounded-xl border-2 border-cyan-100 shadow-md grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6'>  */}
+				{/* V2 */}
+				<div className='bg-cyan-100 rounded-xl border-2 border-cyan-100 shadow-md grid grid-cols-1 gap-3 md:gap-6 p-2 md:p-6'>
 					{/* Map through all the posts but display only featured posts */}
 
 					{posts.map((post) => {
 						if (post.featured) {
 							return (
-								<Link key={post._id} href={`/post/${post.slug.current}`}>
-									<div className='text-white h-96 border-2 bg-gradient-to-r from-teal-700 to-cyan-700 shadow-xl border-teal-700  rounded-lg group cursor-pointer overflow-hidden'>
+								<Link
+									className='border-b border-slate-400 pb-6'
+									key={post._id}
+									href={`/post/${post.slug.current}`}
+								>
+									<div className='sm:flex sm:flex-row-reverse sm:justify-center sm:items-center text-white h-96 sm:h-72 border-2 bg-gradient-to-r from-teal-700 to-cyan-700 shadow-xl border-teal-700 rounded-lg group cursor-pointer overflow-hidden'>
 										<Image
 											unoptimized={true}
 											width='1000'
 											height='1000'
-											className='h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out'
+											className='h-60 sm:h-full w-full sm:w-[60vw] object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out'
 											src={urlFor(post.mainImage).url()!}
 											alt='article'
 										/>
-										<div className='flex justify-between p-5 '>
+										<div className='flex justify-between p-5 sm:w-[40vw]'>
 											<div>
 												<p className='text-lg font-bold'>{post.title}</p>
 												<p className='text-xs'>
